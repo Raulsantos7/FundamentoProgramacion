c6cd91703202e709acf06dbee372c5268bb30d78
raul santos <>
2022-08-25 17:39:26 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HOLAMUNDO/main.c
@@ -0,0 +1,14 @@
+//Hola Mundo
+//Raul Santos
+//25-agosto-2022
+//version 1
+//biblioteca:codigo de una persona que nos lo libero 
+#include <stdio.h>
+/*hola raul
+santos */
+//funcion principal 
+int main(void) {
+  //funcion de impresion en pantalla
+  printf("Hola Mundo\n");
+  return 0;
+}
